5746ee20957fe201a8d511182f5c35fb6a4d326f
petros <petroshambirr@gmail.com>
2026-09-14 19:33:37 +0300

    fix imgi files

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -22,8 +22,8 @@ const compressImageFile = (file) => {
         let width = img.width;
         let height = img.height;
 
-        const MAX_WIDTH = 2000;
-        const MAX_HEIGHT = 2000;
+        const MAX_WIDTH = 3000;
+        const MAX_HEIGHT = 3000;
         if (width > height) {
           if (width > MAX_WIDTH) {
             height *= MAX_WIDTH / width;
@@ -47,7 +47,7 @@ const compressImageFile = (file) => {
             lastModified: Date.now(),
           });
           resolve(compressedFile);
-        }, 'image/jpeg', 0.9);
+        }, 'image/jpeg', 1);
       };
     };
   });
