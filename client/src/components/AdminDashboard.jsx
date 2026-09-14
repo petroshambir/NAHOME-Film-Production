ec5bd80914294045159d6e1619b5e7d6a7e760b9
petros <petroshambirr@gmail.com>
2026-09-14 19:09:57 +0300

    fix admin dasbord

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -22,8 +22,8 @@ const compressImageFile = (file) => {
         let width = img.width;
         let height = img.height;
 
-        const MAX_WIDTH = 800;
-        const MAX_HEIGHT = 800;
+        const MAX_WIDTH = 1200;
+        const MAX_HEIGHT = 1200;
         if (width > height) {
           if (width > MAX_WIDTH) {
             height *= MAX_WIDTH / width;
@@ -47,7 +47,7 @@ const compressImageFile = (file) => {
             lastModified: Date.now(),
           });
           resolve(compressedFile);
-        }, 'image/jpeg', 0.2);
+        }, 'image/jpeg', 0.5);
       };
     };
   });
